d4c8ae314f7041002e9cb25b5e8403998a5ecc96
Livia89 <liviapriscilla1989@gmail.com>
2024-02-13 01:59:45 +0000

    added url to home

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,30 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar() {
-    return (
-        <nav className="flex bg-black p-6 justify-between text-white">
-            <div className="flex">
-                <Link href='/'><Image src={"/images/pokeball.png"} alt={"PokeNext"} width={40} height={40} /></Link>
-                <h1 className="font-semibold text-4xl mx-3">PokeNext</h1>
-            </div>
-            <ul className="flex items-center">
-                <li>
-                    <Link className="p-3" href='/'>Home</Link>
-                </li>
-                <li>
-                    <Link className="p-3" href='/about'>About</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+	return (
+		<nav className="flex bg-black p-6 justify-between text-white">
+			<Link className="flex" href="/">
+				<Image
+					src={'/images/pokeball.png'}
+					alt={'PokeNext'}
+					width={40}
+					height={40}
+				/>
+				<h1 className="font-semibold text-4xl mx-3">PokeNext</h1>
+			</Link>
+			<ul className="flex items-center">
+				<li>
+					<Link className="p-3" href="/">
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link className="p-3" href="/about">
+						About
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
