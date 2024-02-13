767bc4e0a74dd064d8538d14261adc95e311f45e
Livia89 <liviapriscilla1989@gmail.com>
2024-02-13 00:31:46 +0000

    fix: images broken and update next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['cdn.traction.one']
-  }
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ['raw.githubusercontent.com'],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
