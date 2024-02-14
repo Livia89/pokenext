f850ae732ef37739d196fd9178ac9d4a1a94381f
Livia89 <liviapriscilla1989@gmail.com>
2024-02-14 12:12:24 +0000

    progress bar and font family updated

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -3,8 +3,17 @@ import { faFireAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/router';
 import { Progress } from 'flowbite-react';
+import { Exo } from 'next/font/google';
+
+const exo = Exo({
+	weight: '800',
+	subsets: ['latin'],
+	display: 'swap',
+});
 
 const api = 'https://pokeapi.co/api/v2/pokemon/';
+const apiAbility = 'https://pokeapi.co/api/v2/ability/';
+
 export const getStaticPaths = async () => {
 	const maxPokemons = 150;
 	const res = await fetch(`${api}/?limit=${maxPokemons}`);
@@ -27,7 +36,13 @@ export const getStaticProps = async context => {
 	const res = await fetch(`${api}/${id}`);
 	const data = await res.json();
 
-	return { props: { pokemon: data } };
+	const result = await fetch(`${apiAbility}/${id}`);
+	const abilityData = await result.json();
+	const description = abilityData.effect_entries
+		.map(value => value)
+		.filter(value => value.language.name === 'en')?.[0];
+
+	return { props: { pokemon: { ...data, description: description.effect } } };
 };
 
 // app test case
@@ -38,24 +53,58 @@ export default function Pokemon({ pokemon }) {
 		return <div className="flex justify-center">Loading...</div>;
 	}
 	return (
-		<div className="flex flex-col items-center space-y-3 my-12">
-			<p className="text-4xl max-w-md bg-red-700 py-4 px-8 rounded font-bold text-white">
-				{pokemon.name}
+		<div className="flex flex-col  items-center space-y-16  p-6">
+			<p
+				className={`text-4xl max-w-md bg-red-700 p-6 rounded font-bold text-white  ${exo.className}`}
+			>
+				{pokemon.name.toUpperCase()}
 			</p>
-			<div>
-				<Image
-					className="mb-6"
-					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`}
-					alt={pokemon.name}
-					width={250}
-					height={250}
-				/>
-			</div>
-			<div className="w-72 text-center flex flex-col space-y-4">
-				<div>
-					<h3 className="text-lg font-bold ">Id: </h3>
-					<p>#{pokemon.id}</p>
+			<div className={`text-lg text-center`}>{pokemon.description}</div>
+			<div className="w-11/12">
+				<div className="   items-center  flex md:flex-row  flex-col-reverse ">
+					<div className="space-y-4 w-full  ">
+						<h2 className="font-semibold text-2xl text-center">Base stats</h2>
+
+						{pokemon?.stats.map((type, index) => {
+							const { base_stat, stat } = { ...type };
+							const newName = stat.name.split('-').join(' ');
+							const colors = [
+								'red',
+								'pink',
+								'yellow',
+								'green',
+								'indigo',
+								'blue',
+								'indigo',
+								'purple',
+							];
+							return (
+								<div key={index}>
+									<div className=" text-left text-sm font-semibold text-gray-500">
+										{newName.charAt(0).toUpperCase() + newName.slice(1)}
+									</div>
+									<Progress
+										progress={base_stat}
+										color={colors[index]}
+										size="md"
+									/>
+								</div>
+							);
+						})}
+					</div>
+					<div className="w-full flex items-center text-center">
+						<Image
+							className="ml-auto"
+							src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+							alt={pokemon.name}
+							width={360}
+							height={360}
+						/>
+					</div>
 				</div>
+			</div>
+
+			<div className="w-72 text-center mt-12 flex flex-col space-y-8">
 				<div>
 					<h3 className="text-lg font-bold ">Tipo: </h3>
 					<div className="flex space-x-2  items-center justify-center ">
@@ -86,32 +135,6 @@ export default function Pokemon({ pokemon }) {
 						<span>{pokemon.weight / 10} kg</span>
 					</div>
 				</div>
-			</div>
-			<div className="md:w-1/2 2xl:1/3 w-full space-y-4 px-6">
-				<h2 className="font-semibold text-2xl my-6 text-center">Base stats</h2>
-
-				{pokemon?.stats.map((type, index) => {
-					const { base_stat, stat } = { ...type };
-					const newName = stat.name.split('-').join(' ');
-					const colors = [
-						'red',
-						'pink',
-						'yellow',
-						'green',
-						'indigo',
-						'blue',
-						'indigo',
-						'purple',
-					];
-					return (
-						<div key={index}>
-							<div className=" text-left text-sm font-semibold text-gray-500">
-								{newName.charAt(0).toUpperCase() + newName.slice(1)}
-							</div>
-							<Progress progress={base_stat} color={colors[index]} size="md" />
-						</div>
-					);
-				})}
 			</div>
 		</div>
 	);
