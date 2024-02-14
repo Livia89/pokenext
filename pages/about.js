a0a31f1530f37cadc86800be8e4966995f73f0de
Livia89 <liviapriscilla1989@gmail.com>
2024-02-14 12:10:18 +0000

    fontFamily exo added

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,16 @@
 import Image from 'next/image';
+import { Exo } from 'next/font/google';
+
+const exo = Exo({
+	weight: '800',
+	subsets: ['latin'],
+	display: 'swap',
+});
 
 export default function about() {
 	return (
 		<div className="flex flex-col items-center">
-			<h1 className="font-semibold text-4xl m-12 ">About</h1>
+			<h1 className={`font-semibold text-4xl m-12 ${exo.className}`}>About</h1>
 			<Image
 				src="/images/charizard.png"
 				className="mb-6"
@@ -11,12 +18,12 @@ export default function about() {
 				width={300}
 				height={300}
 			/>
-			<p className="text-center max-w-3xl">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dignissim,
-				ex nec tincidunt lacinia, risus justo pharetra urna, vel rutrum metus
-				nulla sit amet risus. Nunc lectus nulla, vehicula id orci at, dignissim
-				porttitor nulla. Integer sagittis nibh magna, eu aliquet libero
-				condimentum nec
+			<p className={`text-center max-w-7xl text-2xl  `}>
+				Pokémon are mysterious creatures filled with many secrets. Some Pokémon
+				live alongside humans and some live in the wild in grassy fields, caves,
+				or the sea, but much about their ecology that remains unknown. One of
+				their main features is that they can be caught using a Poké Ball, which
+				allows them to be carried around.
 			</p>
 		</div>
 	);
